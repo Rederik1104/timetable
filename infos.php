b96959d9d19c0dddb862b8ada09ee9da50cac849
Rederik1104 <116882326+Rederik1104@users.noreply.github.com>
2024-03-11 13:03:56 +0100

    Aktualisieren von infos.php show all teacher

--- infos.php
+++ infos.php
@@ -52,6 +52,43 @@
               <a href="teacher_add.php" class="card-link">Card link</a>
             </div>
           </div>
+					<?php
+						$dbconfig['host'] = 'localhost';
+						$dbconfig['user'] = 'root';
+           $dbconfig['base'] = 'login';
+           $dbconfig['pass'] = '';
+           $dbconfig['char'] = 'utf8';
+                          
+           try {
+            $pdo = new
+            PDO('mysql:host='.$dbconfig['host'].';dbname='.$dbconfig['base'].';charset='.$dbconfig['char'].';',
+            $dbconfig['user'], $dbconfig['pass']);
+           }
+           catch(PDOException $e) {
+            exit('Unable to connect Database.');
+           }
+						
+						Session_start();
+						$id = $_Session("id");
+						$stmt = "SELECT * FROM teacher WHERE id = $id"
+						while($row = $stmt->fetch()){
+							<div class="card_add">
+          			<div class="card" style="width: 18rem;">
+            			<div class="card-body">
+              			<h5 class="card-title"><?php echo "row["nachname"], row["vorname"]" ?></h5>
+              			<p class="card-text">Some quick information on your teacher</p>
+            			</div>
+            		<ul class="list-group list-group-flush">
+              		<li class="list-group-item"><?php echo row["subject1"] ?></li>
+              		<li class="list-group-item"><?php echo row["subject2"] ?></li>
+            		</ul>
+            		<div class="card-body">
+              		<a href="teacher_add.php" class="card-link">Card link</a>
+            		</div>
+          		</div>
+						}
+					?>
+					
           <button type="button" class="btnbtn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="margin-top: 180px; margin-left: 50px; width: 80px; height: 80px; background: none; border: none;"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
@@ -135,6 +172,7 @@
               </div>
             </div>
           </div>
+		
           
   </main>
     
